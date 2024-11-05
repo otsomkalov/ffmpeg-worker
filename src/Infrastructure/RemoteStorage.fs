2d904fee2e788035c0221bd157ea019e18e49a2f
Oleg Tsomkalov <16070015+otsomkalov@users.noreply.github.com>
2024-11-05 15:19:58 +0200

    Remove some functions

--- src/Infrastructure/RemoteStorage.fs
+++ src/Infrastructure/RemoteStorage.fs
@@ -9,19 +9,13 @@ open Domain.Workflows
 open Infrastructure.Core
 
 module RemoteStorage =
-  type ContainerType =
-    | Input
-    | Output
-
-  let getContainerClient (client: BlobServiceClient) (storageSettings: Settings.StorageSettings) =
-    function
-    | Input -> storageSettings.Input.Container
-    | Output -> storageSettings.Output.Container
-    >> client.GetBlobContainerClient
-
-  let downloadFile client settings (loggerFactory: ILoggerFactory) : RemoteStorage.DownloadFile =
+  let downloadFile
+    (client: BlobServiceClient)
+    (settings: Settings.StorageSettings)
+    (loggerFactory: ILoggerFactory)
+    : RemoteStorage.DownloadFile =
     let logger = loggerFactory.CreateLogger(nameof RemoteStorage.DownloadFile)
-    let inputContainerClient = getContainerClient client settings Input
+    let inputContainerClient = client.GetBlobContainerClient(settings.Input.Container)
 
     fun inputFileName ->
       task {
@@ -32,14 +26,22 @@ module RemoteStorage =
 
         do! blobClient.DownloadToAsync(downloadedFile.Path) |> Task.map ignore
 
-        Logf.logfi logger "Remote input file %s{InputFileName} downloaded to local %s{DownloadedFileName}" inputFileName downloadedFile.FullName
+        Logf.logfi
+          logger
+          "Remote input file %s{InputFileName} downloaded to local %s{DownloadedFileName}"
+          inputFileName
+          downloadedFile.FullName
 
         return downloadedFile
       }
 
-  let uploadFile client settings (loggerFactory: ILoggerFactory) : RemoteStorage.UploadFile =
+  let uploadFile
+    (client: BlobServiceClient)
+    (settings: Settings.StorageSettings)
+    (loggerFactory: ILoggerFactory)
+    : RemoteStorage.UploadFile =
     let logger = loggerFactory.CreateLogger(nameof RemoteStorage.UploadFile)
-    let outputContainerClient = getContainerClient client settings Output
+    let outputContainerClient = client.GetBlobContainerClient(settings.Output.Container)
 
     fun file ->
       task {
@@ -50,9 +52,13 @@ module RemoteStorage =
         Logf.logfi logger "Converted file %s{ConvertedFileName} uploaded" file.FullName
       }
 
-  let deleteFile client settings (loggerFactory: ILoggerFactory) : RemoteStorage.DeleteFile =
+  let deleteFile
+    (client: BlobServiceClient)
+    (settings: Settings.StorageSettings)
+    (loggerFactory: ILoggerFactory)
+    : RemoteStorage.DeleteFile =
     let logger = loggerFactory.CreateLogger(nameof RemoteStorage.DeleteFile)
-    let inputContainerClient = getContainerClient client settings Input
+    let inputContainerClient = client.GetBlobContainerClient(settings.Input.Container)
 
     fun name ->
       task {
