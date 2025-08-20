0c08a416ab8ed7814e76ac82af925cd72f6e73b7
Oleg Tsomkalov <16070015+otsomkalov@users.noreply.github.com>
2025-08-20 22:35:29 +0300

    Move RemoveStorage functions into an interface (#70)

---
+++ src/Domain/Repos.fs
@@ -0,0 +1,10 @@
+﻿namespace Domain
+
+open System.Threading.Tasks
+open Domain.Core
+
+module Repos =
+  type IRemoteStorage =
+    abstract DownloadFile: string -> Task<File>
+    abstract UploadFile: File -> Task<unit>
+    abstract DeleteFile: string -> Task<unit>
