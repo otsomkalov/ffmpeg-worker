38d4f6b265869c1c2dad61261581db8bedb238fd
Oleg Tsomkalov <16070015+otsomkalov@users.noreply.github.com>
2025-09-23 18:46:18 +0300

    Remove special characters from resulting file name

--- src/Infra/Core.fs
+++ src/Infra/Core.fs
@@ -3,15 +3,19 @@
 open System.IO
 open shortid
 open Domain.Core
+open shortid.Configuration
 
 [<RequireQualifiedAccess>]
 module File =
-  let create extension =
-    let name = ShortId.Generate()
-    let fileNameWithExtension = sprintf "%s%s" name extension
-    let filePath = Path.Combine(Path.GetTempPath(), fileNameWithExtension)
+  let create =
+    let idGenerationOptions = GenerationOptions(useSpecialCharacters = false, length = 12)
 
-    { Name = name
-      FullName = fileNameWithExtension
-      Extension = extension
-      Path = filePath }
+    fun extension ->
+      let name = ShortId.Generate(idGenerationOptions)
+      let fileNameWithExtension = sprintf "%s%s" name extension
+      let filePath = Path.Combine(Path.GetTempPath(), fileNameWithExtension)
+
+      { Name = name
+        FullName = fileNameWithExtension
+        Extension = extension
+        Path = filePath }
